bee8e254e73bf354f9dff99b6b853467d7bc2cb2
SILVA, H. N <hudsonnsilva@gmail.com>
2024-07-05 17:20:39 -0300

    Add files via upload

---
+++ 08_inserindo_registros_nas_tabelas.sql
@@ -0,0 +1,14 @@
+/* Inserindo Registros nas tabelas*/
+insert into `bd_caso_estudo_vendas`.`tb_depto` (depto_cod,depto_desc)
+values (1,'Vendas');
+insert into `bd_caso_estudo_vendas`.`tb_depto` (depto_cod,depto_desc)
+values (2,'Administrativo');
+insert into `bd_caso_estudo_vendas`.`tb_depto` (depto_cod,depto_desc)
+values (3,'Marketing');
+insert into `bd_caso_estudo_vendas`.`tb_depto` (depto_cod,depto_desc)
+values (4,'Financeiro');
+insert into `bd_caso_estudo_vendas`.`tb_depto` (depto_cod,depto_desc)
+values (5,'Pessoal');
+/*Consultando Registros*/
+
+select * from tb_depto
