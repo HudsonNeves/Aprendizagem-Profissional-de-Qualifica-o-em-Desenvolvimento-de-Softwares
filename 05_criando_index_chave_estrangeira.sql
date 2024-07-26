6d9150cb75aa8907d764393fc1a2b55453ff6fd5
SILVA, H. N. <hudsonnsilva@gmail.com>
2024-07-26 11:03:29 -0300

    Update 05_criando_index_chave_estrangeira.sql

--- 05_criando_index_chave_estrangeira.sql
+++ 05_criando_index_chave_estrangeira.sql
@@ -24,7 +24,7 @@ add index `fk_prod_forn_idx`(`prod_forn_cod` ASC);
 /*Criando chave estrangeira*/
 alter table `bd_caso_estudo_vendas`.`tb_prod`
 add constraint `fk_prod_forn`
-	foreign key (`forn_prod_cod`)
+	foreign key (`prod_forn_cod`)
 	references `bd_caso_estudo_vendas`.`tb_forn` (`forn_cod`)
     on delete no action
     on update no action;
